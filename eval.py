2731d526318b168ca2b47a753a82edac1166fc0d
Jessie-jx <61259449+Jessie-jx@users.noreply.github.com>
2020-06-20 14:32:58 +0800

    Update eval.py

--- eval.py
+++ eval.py
@@ -22,7 +22,7 @@ save_score1 = np.zeros((1, 1))
 predict_value = []
 net = DenseNet(drop_rate=0.5).to(device)
 
-net.load_state_dict(torch.load('/best paras.pkl'))
+net.load_state_dict(torch.load('best paras.pkl'))
 with torch.no_grad():
     for data in test_loader:
         Xtest = data
@@ -39,7 +39,7 @@ with torch.no_grad():
 
 import csv
 #stag_01_submit
-csvFile = open("/test_submit.csv", "w",newline='')            #创建csv文件
+csvFile = open("test_submit.csv", "w",newline='')            #创建csv文件
 writer = csv.writer(csvFile)                  #创建写的对象
 #先写入columns_name
 writer.writerow(["name","predicted"])     #写入列的名称
